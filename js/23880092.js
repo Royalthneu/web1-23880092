b9a62cda903303f19fa9123dd8826dfa8afaef0a
Royal <trungdt.neu@gmail.com>
2025-05-24 21:27:28 +0700

    sendmail3

--- js/23880092.js
+++ js/23880092.js
@@ -1,12 +1,31 @@
 const API = 'https://web1-api.vercel.app/api';
+const AUTHENTICATE_API = 'https://web1-api.vercel.app/users';
 
 async function loadData(request, templateId, viewId) {
-    const reponse = await fetch(`${API}/${request}`);
-    const data = await reponse.json();
+    const response = await fetch(`${API}/${request}`);
+    const data = await response.json();
 
     var source = document.getElementById(templateId).innerHTML;
     var template = Handlebars.compile(source);
     var context = { data : data };
     var view = document.getElementById(viewId);
     view.innerHTML = template(context);    
+}
+
+async function getAuthoricateToken(username, password) {
+    let response = await fetch('{AUTHENTICATE_API}/authenticate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({username, password})
+    });
+    let result = response.json();
+    if(response.status === 200) {
+        return result.token;
+    }
+    else {
+        throw new Error('result.message');
+    }
 }
